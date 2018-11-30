b5777a0dbf21a8385664ab7e8878b455a419db57
Kyle Larose <kyle@agilicus.com>
2018-11-30 14:00:34 -0500

    Paralle test working
    This seems to be the bare minimum for the paralell test.
    
    Signed-off-by: Kyle Larose <kyle@agilicus.com>

--- test/integration/src_transparent_integration_test.cc
+++ test/integration/src_transparent_integration_test.cc
@@ -62,5 +62,62 @@ TEST_F(SrcTransparentIntegrationTest, backToBackConnectionsDifferentIp) {
   EXPECT_EQ(expected_ip->ip()->ipv4()->address(),
             first_upstream_remote_address_->ip()->ipv4()->address());
 }
+TEST_F(SrcTransparentIntegrationTest, parallelConnectionsSameIp) {
+  enableSrcTransparency(0);
+
+  auto creator = getSourceIpConnectionCreator("127.0.0.2");
+  initialize();
+  Http::TestHeaderMapImpl request_headers{{":method", "GET"},
+                                          {":path", "/test/long/url"},
+                                          {":scheme", "http"},
+                                          {":authority", "host"},
+                                          {"x-lyft-user-id", "123"}};
+  auto codec_client1 = makeHttpConnection(creator());
+  auto codec_client2 = makeHttpConnection(creator());
+  auto response1 = codec_client1->makeHeaderOnlyRequest(request_headers);
+  auto response2 = codec_client2->makeHeaderOnlyRequest(request_headers);
+  waitForNextUpstreamRequest();
+  auto first_request = std::move(upstream_request_);
+  // Send response headers, and end_stream if there is no response body.
+  auto first_ip = first_upstream_remote_address_;
+  auto first_upstream_connection = std::move(fake_upstream_connection_);
+  waitForNextUpstreamRequest();
+  auto second_request = std::move(upstream_request_);
+  auto second_ip = first_upstream_remote_address_;
+  auto second_upstream_connection = std::move(fake_upstream_connection_);
+
+  first_request->encodeHeaders(default_response_headers_, true /* no body => close */);
+  second_request->encodeHeaders(default_response_headers_ , true /* no body => close */);
+  response1->waitForEndStream();
+  response2->waitForEndStream();
+  EXPECT_TRUE(first_request->complete());
+  EXPECT_TRUE(second_request->complete());
+
+  EXPECT_TRUE(response1->complete());
+  EXPECT_TRUE(response2->complete());
+  {
+  #if 0
+    // from cleanupUpstreamAndDownstream()
+    auto result = first_upstream_connection->close();
+    RELEASE_ASSERT(result, result.message());
+    result = second_upstream_connection->close();
+    RELEASE_ASSERT(result, result.message());
+    result = first_upstream_connection->waitForDisconnect();
+    RELEASE_ASSERT(result, result.message());
+    result = second_upstream_connection->waitForDisconnect();
+    RELEASE_ASSERT(result, result.message());
+  #endif
+    codec_client1->close();
+    codec_client2->close();
+  }
+  {
+    //from ~HttpIntegrationTest
+    first_request.reset();
+    second_request.reset();
+    first_upstream_connection.reset();
+    second_upstream_connection.reset();
+    fake_upstreams_.clear();
+  }
+}
 
 }
